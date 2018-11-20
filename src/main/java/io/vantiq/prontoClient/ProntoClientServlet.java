ff06f9bf5b7bf288d189e9e9f124a5c2dccba4c6
Namir Fawaz <namirfawaz@Namirs-MBP-2.attlocal.net>
2018-11-20 15:17:36 -0800

    Cleaned up comments and added functionality for auth login.

--- src/main/java/io/vantiq/prontoClient/ProntoClientServlet.java
+++ src/main/java/io/vantiq/prontoClient/ProntoClientServlet.java
@@ -63,7 +63,7 @@ public class ProntoClientServlet extends HttpServlet {
         String catalogName = request.getParameter(PARAM_CATALOG_NAME);
         String eventPath = request.getParameter(PARAM_EVENT_PATH);
         
-        // Try to authenticate with VANTIQ if values are submitted
+        // Try to authenticate with VANTIQ if username/password are submitted
         if (submitPass) {
             // Using VANTIQ SDK to authenticate
             String username = request.getParameter(PARAM_USERNAME);
@@ -91,18 +91,35 @@ public class ProntoClientServlet extends HttpServlet {
                 request.setAttribute("managerData", managerNames);
                 RequestDispatcher view = request.getRequestDispatcher("allCatalogs.jsp");
                 view.forward(request, response);
-            } else {              
-                // Display the appropriate view
+            } else {
+                // Username/password were invalid, display the appropriate view
                 request.setAttribute("invalidCreds", true);
                 RequestDispatcher view = request.getRequestDispatcher("index.jsp");
                 view.forward(request, response);
             }
         } else if (submitAuth) {
-            // Using VANTIQ SDK to authenticate
+            // Using VANTIQ SDK to authenticate with access token
             String authToken = request.getParameter(PARAM_AUTHTOKEN);
             vantiq.setAccessToken(authToken);
             
+            // Selecting all of the managers for given user
+            // Create object to be used as filter for VANTIQ SDK Select
+            HashMap<String,String> selectFilter = new HashMap<String,String>();
+            selectFilter.put("ars_properties.manager", "true");
+            VantiqResponse managerResponse = vantiq.select("system.nodes", null, selectFilter, null);
+            
+            // Get list of the managers with their meta data
+            ArrayList<JsonObject> managerResponseBody = (ArrayList<JsonObject>) managerResponse.getBody();
+            
+            // Iterating through the JSON representation for each manager and storing namespace names
+            ArrayList<String> managerNames = new ArrayList<String>();
+            for (int i = 0; i < managerResponseBody.size(); i++) {
+                String managerName = managerResponseBody.get(0).get("name").getAsString();
+                managerNames.add(managerName);
+            }            
+            
             // Display the appropriate view
+            request.setAttribute("managerData", managerNames);
             RequestDispatcher view = request.getRequestDispatcher("allCatalogs.jsp");
             view.forward(request, response);
         } else if (viewCatalog) {
@@ -118,7 +135,7 @@ public class ProntoClientServlet extends HttpServlet {
             ArrayList<JsonObject> subscriberArray = (ArrayList<JsonObject>) subscriberResponse.getBody();
             ArrayList<JsonObject> publisherArray = (ArrayList<JsonObject>) publisherResponse.getBody();
             
-            // Iterate over JsonArray to store in ArrayList since .jsp can't iterate over JsonArray
+            // Iterate over JsonArray and store in ArrayList (.jsp file can't iterate over JsonArray)
             ArrayList<JsonObject> catalogArrayList = new ArrayList<JsonObject>();
             for (int i = 0; i < catalogArray.size(); i++) {
                 JsonObject elem = catalogArray.get(i).getAsJsonObject();
@@ -145,7 +162,7 @@ public class ProntoClientServlet extends HttpServlet {
             RequestDispatcher view = request.getRequestDispatcher("catalog.jsp");
             view.forward(request, response);
         } else if (viewLive) {
-            // First unsubscribe, then subscribe
+            // Subscribing to live events, (first unsubscribe, then subscribe)
             vantiq.unsubscribeAll();
             vantiq.subscribe(Vantiq.SystemResources.TOPICS.value(), eventPath, null, new SubscriptionOutputCallback());
         } else {
